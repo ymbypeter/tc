dc806b42e7e7c7c1d4dd36ec1bbcb8cfa09f0edd
Barry5233 <47845001+Barry5233@users.noreply.github.com>
2020-09-07 13:47:51 +0800

    0907
    123

---
+++ 前端程式/hello.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button @click="status = !status">aaa</button>
+    </div>
+</template>
+
+<script>
+export default{
+    data: () => {
+        return {
+            status: false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
